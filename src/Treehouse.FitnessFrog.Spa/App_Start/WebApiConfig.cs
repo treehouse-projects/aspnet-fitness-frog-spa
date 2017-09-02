219d031a55dc8c48a640abbf9cf85a77be95e928
James Churchill <james@smashdev.com>
2017-09-01 17:52:32 -0700

    Configured the default route

--- src/Treehouse.FitnessFrog.Spa/App_Start/WebApiConfig.cs
+++ src/Treehouse.FitnessFrog.Spa/App_Start/WebApiConfig.cs
@@ -10,7 +10,10 @@ namespace Treehouse.FitnessFrog.Spa
     {
         public static void Register(HttpConfiguration config)
         {
-
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional });
         }
     }
 }
