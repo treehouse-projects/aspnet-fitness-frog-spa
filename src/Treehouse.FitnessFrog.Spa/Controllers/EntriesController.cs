cf9452ff1c0abe2c4b6897622cbb0f4662417651
James Churchill <james@smashdev.com>
2017-09-06 14:16:04 -0700

    Added the Entries controller

---
+++ src/Treehouse.FitnessFrog.Spa/Controllers/EntriesController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Treehouse.FitnessFrog.Shared.Models;
+
+namespace Treehouse.FitnessFrog.Spa.Controllers
+{
+    public class EntriesController : ApiController
+    {
+        public IEnumerable<Entry> Get()
+        {
+            var activityBiking = new Activity() { Name = "Biking" };
+
+            return new List<Entry>()
+            {
+                new Entry(2017, 1, 2, activityBiking, 10.0m),
+                new Entry(2017, 1, 3, activityBiking, 12.2m)
+            };
+        }
+
+        public Entry Get(int id)
+        {
+            return null;
+        }
+
+        public void Post()
+        {
+
+        }
+
+        public void Put()
+        {
+
+        }
+
+        public void Delete()
+        {
+
+        }
+    }
+}
